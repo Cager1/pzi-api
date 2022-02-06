676b59dc6477acdd20a4bc43f8f6d04dc2afc732
Cager1 <filip.crnogorac98@hotmail.com>
2022-02-06 01:48:15 +0100

    Role management CRUD model

---
+++ app/Http/Controllers/RoleController.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Role;
+use Illuminate\Http\Request;
+
+class RoleController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return Role[]|\Illuminate\Database\Eloquent\Collection|\Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+        return Role::all();
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $request->validate([
+            'name' => 'required',
+        ]);
+        return Role::create($request->all());
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        return Role::find($id);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $role = Role::find($id);
+        $role->update($request->all());
+        return $role;
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function destroy($id)
+    {
+        $name = Role::find($id)->name;
+        Role::destroy($id);
+        return response()->json([
+            'message' => $name." role deleted",
+        ]);
+    }
+}
--- app/Models/User.php
+++ app/Models/User.php
@@ -47,4 +47,8 @@ class User extends Authenticatable
     public function setPasswordAttribute($password) {
         $this->attributes['password'] = bcrypt($password);
     }
+
+    public function role() {
+        return $this->belongsTo(Role::class);
+    }
 }
